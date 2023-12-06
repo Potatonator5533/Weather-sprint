1423147f7cd76e3c91436b8f073253789b940c37
Potatonator5533 <josue04rosales@gmail.com>
2023-12-06 15:13:50 -0800

    API/ search update

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,37 @@
 let favList = document.getElementById("favList");
 let home = document.getElementById("home");
-let dataHere = document.getElementById("dataHere");
+// let dataHere = document.getElementById("dataHere");
 let day = document.getElementById("day");
-let city = document.getElementById("city");
+let cityText = document.getElementById("cityText");
 let fav = document.getElementById("fav");
 let temp = document.getElementById("temp");
 let icon = document.getElementById("icon");
 let iconText = document.getElementById("iconText");
+let userCity = document.getElementById("userCity");
+let submitBtn = document.getElementById("submitBtn");
 
 
-favList.addEventListener('click', function (e) {
-    GetData();
+submitBtn.addEventListener('click', function (e) {
+    findCity(userCity);
 });
 
-function GetData() {
-    fetch("https://api.adviceslip.com/advice").then(
+function findCity(userCity) {
+    fetch(`https://api.api-ninjas.com/v1/city?name=${userCity}`).then(
         response => response.json()
     ).then(
-        // data => console.log(data)
-        data => dataHere.innerText = data.slip.advice   
+        data => cityText.innerText = data.name
     )
-};
+}
+
+
+
+// function GetData() {
+//     fetch("https://randomuser.me/api/").then(
+//         response => response.json()
+//     ).then(
+//         // data => console.log(data)
+//         data => cityText.innerText = data.results.location 
+//     )
+// };
+
+// GetData();
