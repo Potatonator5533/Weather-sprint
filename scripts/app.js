d4a41c5da7bc97970e10431a901b3c464c4572f3
Potatonator5533 <josue04rosales@gmail.com>
2023-12-06 11:50:47 -0800

    Ids

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
 let favList = document.getElementById("favList");
+let home = document.getElementById("home");
 let dataHere = document.getElementById("dataHere");
+let day = document.getElementById("day");
+let city = document.getElementById("city");
+let fav = document.getElementById("fav");
+let temp = document.getElementById("temp");
+let icon = document.getElementById("icon");
+let iconText = document.getElementById("iconText");
+
 
 favList.addEventListener('click', function (e) {
     GetData();
